d86259f2b7cae6374a9d8a96a1b76bbe5fc5be30
Julia Lojek <julialojek@gmail.com>
2020-06-03 12:12:43 +0200

    selectors

--- src/store/modules/selectors.ts
+++ src/store/modules/selectors.ts
@@ -1,5 +1,20 @@
 import { StateModel } from "./models";
+import { OfferModel } from "../../models/models";
 
 export const selectAllOffers = (state: StateModel) => {
   return state.offersList;
+};
+
+export const selectFilteredOffers = (
+  state: StateModel,
+  searchPhrase: string
+) => {
+  return state.offersList.filter((offer: OfferModel) => {
+    return (
+      offer.city.toLowerCase().includes(searchPhrase.toLowerCase()) ||
+      offer.lang.join("").toLowerCase().includes(searchPhrase.toLowerCase()) ||
+      offer.req.join("").toLowerCase().includes(searchPhrase.toLowerCase()) ||
+      offer.notes.toLowerCase().includes(searchPhrase.toLowerCase())
+    );
+  });
 };
