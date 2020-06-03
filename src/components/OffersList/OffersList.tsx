7c67b2354b54c9410213a93a12819caaebd38674
Julia Lojek <julialojek@gmail.com>
2020-06-03 19:36:12 +0200

    adding offers through form works fine and renders in the list view

--- src/components/OffersList/OffersList.tsx
+++ src/components/OffersList/OffersList.tsx
@@ -1,1 +1,27 @@
-export {};
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ACTION_FETCH_OFFERS } from "../../store/modules/actions";
+import { StateModel } from "../../store/modules/models";
+import { selectAllOffers } from "../../store/modules/selectors";
+
+const OffersList: React.FC = () => {
+  const dispatch = useDispatch();
+  const fetchOffersList = () => dispatch(ACTION_FETCH_OFFERS());
+  const offers = useSelector((state: StateModel) => selectAllOffers(state));
+
+  useEffect(() => {
+    fetchOffersList();
+  }, []);
+
+  return <ul>
+    {offers.map(offer => {
+      return <li key={offer.id}>
+        <h4>{offer.city}</h4>
+    <p>{offer.req.join(", ")}</p>
+    <p>{offer.notes}</p>
+      </li>
+    })}
+  </ul>
+};
+
+export default OffersList;
