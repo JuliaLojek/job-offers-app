b6441effd97da318500be673ef66bb191d59ec12
Julia Lojek <julialojek@gmail.com>
2020-06-03 17:25:33 +0200

    form and button styled

--- src/components/AddOfferForm/AddOfferForm.tsx
+++ src/components/AddOfferForm/AddOfferForm.tsx
@@ -3,23 +3,39 @@ import styles from "./AddOfferForm.module.css";
 import Button from "../Button/Button";
 
 const AddOfferForm: React.FC = () => {
+
   return (
-    <div className={styles.formBox}>
+    <div className={styles.mainBox}>
       <h3>Add new offer:</h3>
-      <form>
-        <div className={styles.leftBox}>
+      <form className={styles.formBox}>
+        <div className={styles.inputsBox}>
           <label htmlFor="cityName">City name:</label>
-          <input type="text" id="cityName" placeholder="e.g. Barcelona" />
+          <input
+            type="text"
+            id="cityName"
+            className={styles.input}
+            placeholder="e.g. Barcelona"
+          />
+          <p className={styles.errorBox}></p>
+
           <label htmlFor="req">Job requirements:</label>
           <textarea
             id="req"
+            className={styles.input + " " + styles.textarea}
             placeholder="enter requirements separeted by spaces"
           />
+          <p className={styles.errorBox}></p>
         </div>
-        <div className={styles.rightBox}>
+        <div className={styles.inputsBox}>
           <label htmlFor="notes">Notes:</label>
-          <textarea id="notes" placeholder="additional notes, links, etc." />
-          <Button text="Add!" type="submit" />
+          <textarea
+            id="notes"
+            className={styles.input + " " + styles.textarea}
+            placeholder="additional notes, links, etc."
+          />
+          <p className={styles.errorBox}></p>
+
+          <Button text="Add offer" type="submit" />
         </div>
       </form>
     </div>
