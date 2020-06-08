29f9900856450f99eae32cdef8a44970d589a6e3
Julia Lojek <julialojek@gmail.com>
2020-06-08 18:24:41 +0200

    removing offer modal and buttons work

--- src/components/RemoveModal/RemoveModal.tsx
+++ src/components/RemoveModal/RemoveModal.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./RemoveModal.module.css";
+import { useDispatch } from "react-redux";
+import { ACTION_DELETE_OFFER } from "../../store/modules/actions";
 
 interface RemoveModalProps {
   id: number;
@@ -7,15 +9,32 @@ interface RemoveModalProps {
 }
 
 const RemoveModal: React.FC<RemoveModalProps> = (props) => {
+  const dispatch = useDispatch();
+  const removeOffer = (id: number) => dispatch(ACTION_DELETE_OFFER(id));
+  const handleRemove = (id: number) => {
+    removeOffer(id);
+    props.handleCloseModal(false);
+  }
+
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modal}>
-        <p className={styles.text}>Are you sure you want to remove this offer?</p>
-        <button className={styles.btn + " " + styles.mainBtn}>Remove</button>
-        <button className={styles.btn} onClick={() => props.handleCloseModal(false)}>Cancel</button>
+        <p className={styles.text}>
+          Are you sure you want to remove this offer?
+        </p>
+        <button
+        className={styles.btn + " " + styles.mainBtn}
+        onClick={() => handleRemove(props.id)}
+        >Remove</button>
+        <button
+          className={styles.btn}
+          onClick={() => props.handleCloseModal(false)}
+        >
+          Cancel
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default RemoveModal;
