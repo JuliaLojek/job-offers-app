5a3a063fc76817c3c4d7ac90ba7d82a89b0cdbe7
Julia Lojek <julialojek@gmail.com>
2020-06-15 13:11:46 +0200

    added info works

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./Toast.module.css";
+import { useDispatch } from "react-redux";
+import { ACTION_SHOW_ADDED_INFO } from "../../store/modules/actions";
 
 interface ToastProps {
   type: "added" | "edited" | "removed";
+  isActive: boolean;
 }
 
 const Toast: React.FC<ToastProps> = (props) => {
+  const dispatch = useDispatch();
+  const closeInfo = () => dispatch(ACTION_SHOW_ADDED_INFO(false));
+
   const printText = (type: ToastProps["type"]) => {
     switch (type) {
       case "added":
@@ -17,9 +23,16 @@ const Toast: React.FC<ToastProps> = (props) => {
     }
   };
 
-  return <div className={styles.toast}>
-    <p className={styles.text}>{printText(props.type)}</p>
-  </div>;
+  return (
+    <div
+      className={
+        props.isActive ? [styles.toast, styles.active].join(" ") : styles.toast
+      }
+    >
+      <p className={styles.text}>{printText(props.type)}</p>
+      <p className={styles.btn} onClick={closeInfo}>&#10005;</p>
+    </div>
+  );
 };
 
 export default Toast;
--- src/routes/AddOfferPage.tsx
+++ src/routes/AddOfferPage.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import styles from "./Page.module.css";
 import Form from "../components/Form/Form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { OfferModel } from "../models/models";
-import { ACTION_ADD_OFFER } from "../store/modules/actions";
+import { ACTION_ADD_OFFER, ACTION_SHOW_ADDED_INFO } from "../store/modules/actions";
+import Toast from "../components/Toast/Toast";
+import { selectIsAddedInfoActive } from "../store/modules/selectors";
+import { StateModel } from "../store/modules/models";
 
 const AddOfferPage: React.FC = () => {
   const dispatch = useDispatch();
+  const isToastActive = useSelector((state: StateModel) =>
+    selectIsAddedInfoActive(state)
+  );
+  const addOffer = (newOffer: OfferModel) => {
+    dispatch(ACTION_ADD_OFFER(newOffer));
+    dispatch(ACTION_SHOW_ADDED_INFO(true));
+    setTimeout(() => dispatch(ACTION_SHOW_ADDED_INFO(false)), 3000);
+  };
 
   return (
-    <main className={styles.main}>
-      <h3 className={styles.title}>Add new offer:</h3>
-      <Form
-        mainBtnText="Add offer"
-        mainBtnAction={(newOffer: OfferModel) =>
-          dispatch(ACTION_ADD_OFFER(newOffer))
-        }
-      />
-    </main>
+    <React.Fragment>
+      <main className={styles.main}>
+        <h3 className={styles.title}>Add new offer:</h3>
+        <Form mainBtnText="Add offer" mainBtnAction={addOffer} />
+      </main>
+      <Toast type="added" isActive={isToastActive ? true : false} />
+    </React.Fragment>
   );
 };
 
