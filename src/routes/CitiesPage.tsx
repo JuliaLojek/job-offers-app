b92f37a77dc74ed3467f14e1c915881d2b06efde
Julia Lojek <julialojek@gmail.com>
2020-06-17 15:07:29 +0200

    charts done

--- src/routes/CitiesPage.tsx
+++ src/routes/CitiesPage.tsx
@@ -1,19 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./Page.module.css";
 import { useSelector } from "react-redux";
 import { StateModel } from "../store/modules/models";
 import { selectCityChartData } from "../store/modules/selectors";
+import Chart from "../components/Chart/Chart";
 
 const CitiesPage: React.FC = () => {
   const data = useSelector((state: StateModel) => selectCityChartData(state));
 
-  useEffect(() => {
-    console.log(data);
-  });
-
   return (
     <main className={styles.main}>
-      by cities
+      <Chart data={data} />
     </main>
   )
 }
--- src/routes/RequirementsPage.tsx
+++ src/routes/RequirementsPage.tsx
@@ -1,21 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./Page.module.css";
 import { useSelector } from "react-redux";
 import { StateModel } from "../store/modules/models";
 import { selectReqChartData } from "../store/modules/selectors";
+import Chart from "../components/Chart/Chart";
 
 const RequirementsPage: React.FC = () => {
   const data = useSelector((state: StateModel) => selectReqChartData(state));
 
-  useEffect(() => {
-    console.log(data);
-  });
-
   return (
     <main className={styles.main}>
-      by requirements
+      <Chart data={data} />
     </main>
-  )
-}
+  );
+};
 
 export default RequirementsPage;
