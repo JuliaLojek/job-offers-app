3e602e6b38b37abcc5303b0a89b77244f1e57ddc
Julia Lojek <julialojek@gmail.com>
2020-06-18 11:43:22 +0200

    Updates

---
+++ precache-manifest.5c318433aa67a2e24928e8ace2281c2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5314c412b834c50e175404b7b3030a87",
+    "url": "/job-offers-app/index.html"
+  },
+  {
+    "revision": "970bdb4f3333d408f287",
+    "url": "/job-offers-app/static/css/main.7d48ecc8.chunk.css"
+  },
+  {
+    "revision": "11d68f80c9b21a5a427f",
+    "url": "/job-offers-app/static/js/2.88c1720a.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/job-offers-app/static/js/2.88c1720a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970bdb4f3333d408f287",
+    "url": "/job-offers-app/static/js/main.6f3f1608.chunk.js"
+  },
+  {
+    "revision": "4b3bb90732064b962b11",
+    "url": "/job-offers-app/static/js/runtime-main.6605a75b.js"
+  },
+  {
+    "revision": "5a03cd494f314e5308cfe3eaa264909f",
+    "url": "/job-offers-app/static/media/icons8-edit.5a03cd49.svg"
+  },
+  {
+    "revision": "0a370b3998522f12c5594c9f0186b546",
+    "url": "/job-offers-app/static/media/icons8-remove.0a370b39.svg"
+  }
+]);
