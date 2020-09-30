dfc4432f584640bc52207a68a887d743d01448e1
Julia Lojek <julialojek@gmail.com>
2020-09-30 14:36:40 +0200

    tests

--- src/__tests__/routes/AddOfferPage.test.tsx
+++ src/__tests__/routes/AddOfferPage.test.tsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+import configureStore from "redux-mock-store";
 import AddOfferPage from "../../routes/AddOfferPage";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+// import { createStore } from "redux";
 import { MemoryRouter } from "react-router-dom";
-import reducer from "../../store/modules/reducer";
+// import reducer from "../../store/modules/reducer";
+// import { ACTION_ADD_OFFER } from "../../store/modules/actions";
+import thunk from "redux-thunk";
+// import { OfferModel } from "../../models/models";
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe("<AddOfferPage />", () => {
   test("should display a blank form", () => {
-    const { getByTestId, getByLabelText } = render(
+    const { getByTestId } = render(
       <MemoryRouter>
-        <Provider store={createStore(reducer)}>
+        {/* <Provider store={createStore(reducer)}> */}
+        <Provider store={mockStore()} >
           <AddOfferPage />
         </Provider>
       </MemoryRouter>
     );
-    // const companyInput = getByLabelText(/company name/i);
-    // expect(companyInput).toBeInTheDocument();
     const form = getByTestId("form");
     expect(form).toHaveFormValues({
       companyName: "",
@@ -26,4 +32,41 @@ describe("<AddOfferPage />", () => {
       notes: "",
     });
   });
+
+  // test("after filling in the required fields and submitting the form, the form should be blank again and the toast should appear", () => {
+  //   const { getByTestId, getByLabelText } = render(
+  //     <MemoryRouter>
+  //       <Provider store={mockStore()} >
+  //         <AddOfferPage />
+  //       </Provider>
+  //     </MemoryRouter>
+  //   );
+  //   const form = getByTestId("form");
+  //   const companyInput = getByLabelText(/company/i);
+  //   const cityInput = getByLabelText(/city/i);
+  //   const reqInput = getByLabelText(/req/i);
+  //   fireEvent.change(companyInput, { target: { value: "koala" } });
+  //   fireEvent.change(cityInput, { target: { value: "barcelona" } });
+  //   fireEvent.change(reqInput, { target: { value: "css, js, ts" } });
+  //   const newOfferMock = {
+  //     id: 11,
+  //     company: "koala",
+  //     city: "barcelona",
+  //     req: ["css", "js", "ts"],
+  //     link: "",
+  //     notes: "",
+  //   };
+
+  //   fireEvent.submit(form);
+  //   // expect(addOfferSpy).toHaveBeenCalled();
+
+
+  //   // expect(form).toHaveFormValues({
+  //   //   companyName: "",
+  //   //   cityName: "",
+  //   //   req: "",
+  //   //   link: "",
+  //   //   notes: "",
+  //   // });
+  // })
 });
