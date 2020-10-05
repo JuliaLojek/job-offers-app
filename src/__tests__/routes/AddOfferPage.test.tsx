0cb87bac7576a3ce1b4d2e67d13c562052777e94
Julia Lojek <julialojek@gmail.com>
2020-10-05 12:06:15 +0200

    add new offer tests

--- src/__tests__/routes/AddOfferPage.test.tsx
+++ src/__tests__/routes/AddOfferPage.test.tsx
@@ -3,12 +3,8 @@ import { fireEvent, render } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 import AddOfferPage from "../../routes/AddOfferPage";
 import { Provider } from "react-redux";
-// import { createStore } from "redux";
 import { MemoryRouter } from "react-router-dom";
-// import reducer from "../../store/modules/reducer";
-// import { ACTION_ADD_OFFER } from "../../store/modules/actions";
 import thunk from "redux-thunk";
-// import { OfferModel } from "../../models/models";
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -33,40 +29,34 @@ describe("<AddOfferPage />", () => {
     });
   });
 
-  // test("after filling in the required fields and submitting the form, the form should be blank again and the toast should appear", () => {
-  //   const { getByTestId, getByLabelText } = render(
-  //     <MemoryRouter>
-  //       <Provider store={mockStore()} >
-  //         <AddOfferPage />
-  //       </Provider>
-  //     </MemoryRouter>
-  //   );
-  //   const form = getByTestId("form");
-  //   const companyInput = getByLabelText(/company/i);
-  //   const cityInput = getByLabelText(/city/i);
-  //   const reqInput = getByLabelText(/req/i);
-  //   fireEvent.change(companyInput, { target: { value: "koala" } });
-  //   fireEvent.change(cityInput, { target: { value: "barcelona" } });
-  //   fireEvent.change(reqInput, { target: { value: "css, js, ts" } });
-  //   const newOfferMock = {
-  //     id: 11,
-  //     company: "koala",
-  //     city: "barcelona",
-  //     req: ["css", "js", "ts"],
-  //     link: "",
-  //     notes: "",
-  //   };
+  test("after filling in the required fields and submitting the form, the form should be blank again and the toast should appear", () => {
+    const { getByTestId, getByLabelText, getByText } = render(
+      <MemoryRouter>
+        <Provider store={mockStore()} >
+          <AddOfferPage />
+        </Provider>
+      </MemoryRouter>
+    );
+    const form = getByTestId("form");
+    const companyInput = getByLabelText(/company/i);
+    const cityInput = getByLabelText(/city/i);
+    const reqInput = getByLabelText(/req/i);
 
-  //   fireEvent.submit(form);
-  //   // expect(addOfferSpy).toHaveBeenCalled();
+    fireEvent.change(companyInput, { target: { value: "koala" } });
+    fireEvent.change(cityInput, { target: { value: "barcelona" } });
+    fireEvent.change(reqInput, { target: { value: "css, js, ts" } });
+    
+    fireEvent.submit(form);
 
 
-  //   // expect(form).toHaveFormValues({
-  //   //   companyName: "",
-  //   //   cityName: "",
-  //   //   req: "",
-  //   //   link: "",
-  //   //   notes: "",
-  //   // });
-  // })
+    expect(form).toHaveFormValues({
+      companyName: "",
+      cityName: "",
+      req: "",
+      link: "",
+      notes: "",
+    });
+
+    expect(getByText(/added/i)).toBeInTheDocument();
+  })
 });
